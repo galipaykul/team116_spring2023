e6a460e3ea840666f8433a6328209c629a804dde
galipaykul <131351061+galipaykul@users.noreply.github.com>
2023-05-04 18:33:53 +0300

    team116

---
+++ src/day25_inheritance/Araba.java
@@ -0,0 +1,14 @@
+package day25_inheritance;
+
+public class Araba {
+    Araba() {
+
+        System.out.println("Araba constructor calıştı");
+
+    }
+
+    String model = "Arabaların modeli olur";
+    String motor = "Arabaların motoru olur";
+    String marka = "Arabalarda marka olur";
+
+}
